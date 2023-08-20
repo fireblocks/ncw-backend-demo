d46de32d91f51712df99371b84bb5679df84d27b
Amit Perelstein <amper@fireblocks.com>
2023-08-20 16:39:27 +0300

    update package

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -45,21 +45,21 @@ export class TransactionService {
   async estimate(walletId: string, args: TransactionArguments) {
     const { low, medium, high } = await this.signer.estimateFeeForTransaction(
       args,
-      { NCW: { walletId } },
+      { ncw: { walletId } },
     );
     return { low, medium, high };
   }
 
   async create(walletId: string, args: TransactionArguments) {
     const { id, status } = await this.signer.createTransaction(args, {
-      NCW: { walletId },
+      ncw: { walletId },
     });
     return { id, status };
   }
 
   async cancel(walletId: string, txId: string) {
     const { success } = await this.signer.cancelTransactionById(txId, {
-      NCW: { walletId },
+      ncw: { walletId },
     });
     return { success };
   }
